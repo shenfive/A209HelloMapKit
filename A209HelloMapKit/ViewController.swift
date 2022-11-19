6e5ed8a79f6731df4a0b0fce0c887a09c03c2fe1
shenfive@gmail.com <Danny Shen>
2022-11-19 10:42:21 +0800

    annotation

--- A209HelloMapKit/ViewController.swift
+++ A209HelloMapKit/ViewController.swift
@@ -18,21 +18,32 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        myMap.isScrollEnabled = false
+        myMap.isZoomEnabled = false
+        
+        
+        let latitude:CLLocationDegrees = 25.044271
+        let longitude:CLLocationDegrees = 121.513359
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let xScale:CLLocationDegrees = 0.001
+        let yScale:CLLocationDegrees = 0.001
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
-            
-            
-            let latitude:CLLocationDegrees = 25.044271
-            let longitude:CLLocationDegrees = 121.513359
-            let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-            let xScale:CLLocationDegrees = 0.001
-            let yScale:CLLocationDegrees = 0.001
             let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
             let region:MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
 
             self.myMap.setRegion(region, animated: true)
         })
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: {
+
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            annotation.title = "譯智"
+            annotation.subtitle = "教育訓練中心"
+            self.myMap.addAnnotation(annotation)
+
+        })
         
         
         
@@ -45,7 +56,7 @@ class ViewController: UIViewController {
         case 1:
             myMap.mapType = .satellite
         case 2:
-            myMap.mapType = .hybridFlyover
+            myMap.mapType = .hybrid
             
         default:
             break
